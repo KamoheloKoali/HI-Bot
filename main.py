54eabd5ca197656469be9a05464702a9a1504dcf
Kamohelo Koali <152155388+KamoheloKoali@users.noreply.github.com>
2024-07-30 13:25:18 +0000

    basic seleniumBase script

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import requests
-from bs4 import BeautifulSoup as bs
-# import cloudscraper
+from seleniumbase import Driver
 
 url = "https://intranet.hbtn.io/projects/2348"
 
-#Load the webpage
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0"
-}
-
-page = requests.get(url, headers=headers)
-
-#convert to beautiful soup object
-soup_obj = bs(page.content, "html.parser")
-
-print(page.status_code)
+driver = Driver(uc=True)
+try:
+    driver.get(url)
+    driver.sleep(4)
+finally:
+    driver.quit()
