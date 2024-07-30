bcea00387c3cc22b04f8408fadc268307cc49f9f
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-07-30 17:01:13 +0200

    bypassed cloudfare on holberton intranet

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import requests
-from seleniumbase import Driver
 
-url = "https://intranet.hbtn.io/projects/2348"
+from seleniumbase import SB
 
-driver = Driver(uc=True)
-try:
-    driver.get(url)
-    driver.sleep(4)
-finally:
-    driver.quit()
+url = "https://intranet.hbtn.io"# Replace with the actual URL
+
+"""SB Manager using UC Mode for evading bot-detection."""
+text = ''
+with SB(uc=True, test=True, demo=True) as sb:
+    sb.uc_open_with_reconnect(url, reconnect_time=2)
+    sb.uc_gui_click_captcha()
+    # sb.assert_element("img#captcha-success", timeout=3)
+    sb.set_messenger_theme(location="top_left")
+    sb.post_message("SeleniumBase wasn't detected", duration=5)
+    sb.type("input#user_login", "8754@holbertonstudents.com")
+    sb.type("input#user_password", "ts3sw48K#RGH")
+    sb.click("input.btn.btn-primary")
+    sb.post_message("signed in", duration=5)
+    text = sb.get_text("h3#student-home-current-project-title")
+    sb.post_message("text retrieved", duration=5)
+
+
+print(text)
+
+    
